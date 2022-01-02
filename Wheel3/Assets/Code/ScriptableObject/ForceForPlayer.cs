84f66a69d85002d39c8d38a64f71997dae53c3bc
BiryukovSergey <twc2012@yandex.ru>
2022-01-02 22:15:39 +0300

    движение колеса

---
+++ Wheel3/Assets/Code/ScriptableObject/ForceForPlayer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Code.ScriptableObject
+{
+    [CreateAssetMenu(fileName = "ForceForPlayer", menuName = "ForceForPlayer", order = 0)]
+    public class ForceForPlayer : UnityEngine.ScriptableObject
+    {
+        public float ConstantForce; //постоянная сила
+        public float UphillForce; // в гору
+        public float TiltForce; // c горы
+    }
+}
---
+++ Wheel3/Assets/Code/View/PlayerSpawn.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerSpawn : MonoBehaviour
+{
+    [SerializeField]private GameObject _parentTransform;
+    private GameObject _player;
+
+    private void Awake()
+    {
+        var load = Resources.Load("Sphere") as GameObject;
+        var postionPlayerSpawn = new Vector3(0, 0, 0);
+        _player = Instantiate(load, postionPlayerSpawn, Quaternion.identity,_parentTransform.transform);
+        _player.name = "Player";
+    }
+}
